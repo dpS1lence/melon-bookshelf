5dd695ddedade7f07b9a26702108fb5ca359ea9f
davidpetkov <davidpetkov2006@gmail.com>
2023-07-04 13:49:39 +0300

    Created project

---
+++ src/Services/MelonBookshelfApi/MelonBookchelfApi.Infrastructure/Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelonBookchelfApi.Infrastructure.Data
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+        public IdentityContext(string connectionString)
+           : base(connectionString)
+        {
+        }
+    }
+}
