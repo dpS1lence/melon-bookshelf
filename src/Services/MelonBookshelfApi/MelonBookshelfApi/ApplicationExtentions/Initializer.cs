2dbec91c334b38a0cde68ccdada3890057e10cfb
davidpetkov <davidpetkov2006@gmail.com>
2023-07-11 10:00:56 +0300

    Fix

---
+++ src/Services/MelonBookshelfApi/MelonBookshelfApi/ApplicationExtentions/Initializer.cs
@@ -0,0 +1,30 @@
+﻿using MelonBookchelfApi.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace MelonBookshelfApi.ApplicationExtentions
+{
+    public static class Initializer
+    {
+        public static void Initialize(this IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<BookshelfDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            context.Database.EnsureCreated();
+
+            var roleCheck = roleManager.RoleExistsAsync("Admin").Result;
+            if (!roleCheck)
+            {
+                var roleResult = roleManager.CreateAsync(new IdentityRole("Admin")).Result;
+            }
+
+            IdentityUser user = userManager.FindByEmailAsync("davidpetkov2006@gmail.com").Result;
+            var userRoleCheck = userManager.IsInRoleAsync(user, "Admin").Result;
+            if (!userRoleCheck)
+            {
+                userManager.AddToRoleAsync(user, "Admin").Wait();
+            }
+        }
+    }
+}
--- src/Services/MelonBookshelfApi/MelonBookshelfApi/Services/JwtTokenGenerator.cs
+++ src/Services/MelonBookshelfApi/MelonBookshelfApi/Services/JwtTokenGenerator.cs
@@ -8,20 +8,28 @@ namespace MelonBookshelfApi.Services
 {
     public static class JwtTokenGenerator
     {
-        public static string GenerateToken(IConfiguration configuration, IdentityUser user)
+        public static async Task<string> GenerateToken(IConfiguration configuration, IdentityUser user, UserManager<IdentityUser> userManager)
         {
             var secretKey = configuration["JwtSettings:SecretKey"];
 
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.UTF8.GetBytes(secretKey);
 
+            var roles = await userManager.GetRolesAsync(user);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.Email),
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[]
-                {
-                    new Claim(ClaimTypes.NameIdentifier, user.Id),
-                    new Claim(ClaimTypes.Name, user.Email)
-                }),
+                Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(
                     new SymmetricSecurityKey(key),
@@ -33,5 +41,6 @@ namespace MelonBookshelfApi.Services
 
             return encryptedToken;
         }
+
     }
 }
